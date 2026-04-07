4e6d8442224d1fd783d8d8df8846591ce536df09
NataliiaK <nataliia.kononen@gmail.com>
2026-04-07 11:53:45 +0200

    Fix quick add

--- app/src/main/kotlin/com/basicorganizer/tracker/adapter/TrackingItemAdapter.kt
+++ app/src/main/kotlin/com/basicorganizer/tracker/adapter/TrackingItemAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -44,7 +43,7 @@ class TrackingItemAdapter(
         holder.sentimentIndicator.background.setTint(ContextCompat.getColor(context, sentimentColor))
         
         val isMarked = markedItems[item.id] == true
-        updateCheckState(holder, isMarked)
+        updateCheckState(holder, isMarked, sentimentColor)
         
         holder.btnCheck.setOnClickListener {
             listener.onToggleMark(item)
@@ -60,11 +59,11 @@ class TrackingItemAdapter(
         }
     }
 
-    private fun updateCheckState(holder: ViewHolder, isChecked: Boolean) {
+    private fun updateCheckState(holder: ViewHolder, isChecked: Boolean, sentimentColor: Int) {
         if (isChecked) {
-            holder.btnCheck.setBackgroundResource(R.drawable.check_box_checked)
+            holder.btnCheck.setBackgroundResource(R.drawable.check_box_border)
             holder.checkIcon.visibility = View.VISIBLE
-            holder.checkIcon.setColorFilter(ContextCompat.getColor(context, R.color.text_secondary))
+            holder.checkIcon.background.setTint(ContextCompat.getColor(context, sentimentColor))
         } else {
             holder.btnCheck.setBackgroundResource(R.drawable.check_box_border)
             holder.checkIcon.visibility = View.GONE
@@ -84,6 +83,6 @@ class TrackingItemAdapter(
         val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
         val sentimentIndicator: View = itemView.findViewById(R.id.sentiment_indicator)
         val btnCheck: View = itemView.findViewById(R.id.btn_mark_check)
-        val checkIcon: ImageView = itemView.findViewById(R.id.check_icon)
+        val checkIcon: View = itemView.findViewById(R.id.check_icon)
     }
 }
