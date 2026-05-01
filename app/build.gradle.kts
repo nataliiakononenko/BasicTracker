02791281c265d87067746f7e11f3351af6199233
NataliiaK <nataliia.kononen@gmail.com>
2026-05-01 11:29:27 +0200

    Fix app name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.basicorganizer.tracker"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
